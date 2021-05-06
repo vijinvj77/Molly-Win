e9457530dd7ec18269e3920575916af61b72a047
Vijin V J <vijinvj77@gmail.com>
2021-05-06 18:45:02 +0530

    Added login,registration,header pages

---
+++ header.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark container-fluid">
+        <a class="navbar-brand text-white" href="index.html">Molly-Win</a>
+        <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#about">About</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="login.php">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
